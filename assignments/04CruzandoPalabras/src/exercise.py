182c2571075ed2b33b72fcef29b453342a5c388a
jperezrangel <88467921+jperezrangel@users.noreply.github.com>
2022-10-02 22:48:48 -0500

    Cambios de la Activididad 19

--- assignments/04CruzandoPalabras/src/exercise.py
+++ assignments/04CruzandoPalabras/src/exercise.py
@@ -1,12 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    import math
-    a=input()
-    b=input()
-    ma=math.ceil(len(a)/2)
-    mb=math.ceil(len(b)/2)
-    print(a[0:ma]+b[mb::])
-    print(a[ma::]+b[0:mb])
+    pass
 
 if __name__=='__main__':
     main()
--- assignments/05AmbosExtremos/src/exercise.py
+++ assignments/05AmbosExtremos/src/exercise.py
@@ -1,11 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    s=input()
-    if len(s)<2:
-        resultado=''
-    else:
-        resultado=s[0]+s[1]+s[len(s)-2]+s[len(s)-1]
-    print(resultado)
+    pass
 
 
 if __name__=='__main__':
