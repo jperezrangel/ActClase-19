774fc52dede9f600c5866efe42dd15ca9ee99941
jperezrangel <88467921+jperezrangel@users.noreply.github.com>
2022-10-02 22:50:04 -0500

    Push Actividad 19.

--- assignments/03ReversaBasico/src/exercise.py
+++
@@ -1,9 +0,0 @@
-def main():
-    #escribe tu código abajo de esta línea
-
-    cadena = input("Teclea una cadena de caracteres: ") 
-    print(cadena[::-1].upper())
-    
-
-if __name__=='__main__':
-    main()
